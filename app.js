a3d16ccaad2022bbf82878e79c532cef7a7c0a10
Rj <rikrdo.ortega@gmail.com>
2019-08-02 15:18:27 -0600

    ready to deploy

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ const name = document.querySelector(".title")
 const whatsmyname = document.querySelector(".whats-my-name")
 const subtitle = document.querySelector(".subtitle")
 const modal = document.querySelector(".modal")
-
+const ul = document.querySelector("#ul")
 const aboutMe = document.querySelector(".about-me")
 const guestbook = document.querySelector(".guestbook")
+const form = document.querySelector("form")
+const usersUrl = "http://localhost:3000/users"
+const entriesUrl = "http://localhost:3000/entries"
 
 const buttonAboutMe = document.querySelector("#about-me")
 const buttonGuestbook = document.querySelector("#guestbook")
 const buttonHobbies = document.querySelector("#hobbies")
 const buttonPlayground = document.querySelector("#playground")
+
 
 buttonAboutMe.addEventListener("click", bringAboutMe)
 buttonGuestbook.addEventListener("click", bringGuestbook)
@@ -19,6 +23,22 @@ buttonHobbies.addEventListener("click", bringHobbies)
 buttonPlayground.addEventListener("click", bringPlayground)
 name.addEventListener("click", spin)
 modal.addEventListener("click", toggleModal)
+ul.addEventListener("click", deleteComment)
+
+function deleteComment(event) {
+    if (event.target.id != "deleteButton") return;
+    else if (window.confirm("Are you sure about this?")) {
+
+        const message = event.target.parentNode.parentNode.firstChild.dataset.entryId
+
+        fetch(`${entriesUrl}/${message}`, {
+                method: "delete"
+            })
+            .then(res => res.json())
+        event.target.parentNode.parentNode.remove()
+    }
+}
+
 
 function bringAboutMe() {
     guestbook.classList.remove("open")
@@ -78,4 +98,77 @@ function stahp() {
 
 function toggleModal(event) {
     event.target.classList.remove("open-modal")
+}
+
+fetch(entriesUrl)
+    .then(res => res.json())
+    .then(getInfo)
+
+function getInfo(entries) {
+    entries.data.forEach(entry => {
+        let user = {
+            name: entry.attributes.user_name,
+            id: entry.relationships.user.data.id
+        }
+        let message = {
+            text: entry.attributes.message,
+            id: entry.id
+        }
+        displayInfo(user, message)
+    })
+}
+
+function displayInfo(user, message) {
+    const li = document.createElement("li")
+    li.classList.add("entries")
+    li.innerHTML = `<p data-entry-id="${message.id}" class="entry-message">${message.text}</p><p data-user-id="${user.id}" class="entry-user">- ${user.name} <i id="deleteButton" class="fas fa-times-circle"></i></p>`
+    ul.prepend(li)
+}
+
+form.addEventListener("submit", postIt)
+
+function postIt(event) {
+    event.preventDefault()
+    let userName = document.getElementById("name").value
+    let messageText = document.getElementById("message").value
+    let entryBody = {
+        name: userName
+    }
+    fetch(usersUrl, {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(entryBody)
+        })
+        .then(res => res.json())
+        .then(data => {
+            let userBody = {
+                message: messageText,
+                user_id: data.id
+            }
+            return fetch("http://localhost:3000/entries/", {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userBody)
+            })
+
+        })
+        .then(res => res.json())
+        .then(entry => {
+            let user = {
+                name: entry.data.attributes.user_name,
+                id: entry.data.relationships.user.data.id
+            }
+            let message = {
+                text: entry.data.attributes.message,
+                id: entry.data.id
+            }
+            displayInfo(user, message)
+        })
+    event.target.reset()
 }
